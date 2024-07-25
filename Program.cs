b54475c0bd6ddee4f00d85b6f9c608f84bc4debd
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-07-25 15:09:52 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -2,10 +2,52 @@
 {
     internal class Program
     {
-        
+        static void BubbleSort(ref int[] arr)
+        {
+            int n = arr.Length;
+            bool swap;
+            for (int i = 0; i < n; i++)
+            {
+                swap = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swap = true;
+                    }
+
+                }
+                if (!swap)
+                {
+                    break;
+                }
+
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            int[] arr = { 5, 1, 4, 2, 8 };
+
+            Console.WriteLine("Original array:");
+
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            BubbleSort(ref arr);
+
+            Console.WriteLine("Sorted array:");
+
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
