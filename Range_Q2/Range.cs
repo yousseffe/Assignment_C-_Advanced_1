31b6d2c3e4b43a806f4c97691ced74d3871901ae
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-07-25 16:16:16 +0300

    Q2

---
+++ Range_Q2/Range.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_C__Advanced_1.Range_Q2
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+        public T Min { get; set; }
+        public T Max { get; set; }
+        public Range(T minimum, T maximum)
+        {
+            if (minimum == null || maximum == null)
+                throw new ArgumentNullException(nameof(minimum));
+            if (minimum.CompareTo(maximum) > 0)
+            {
+                throw new ArgumentException("Minimum value cannot be greater than maximum value.");
+            }
+
+            Min = minimum;
+            Max = maximum;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+        public T Length()
+        {
+            dynamic min = Min;
+            dynamic max = Max;
+            return max - min;
+        }
+    }
+}
