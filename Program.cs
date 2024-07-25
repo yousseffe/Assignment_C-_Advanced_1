85c0cf2a2015c45fcc797d282b79ae3d3f1cfe75
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-07-25 16:19:03 +0300

    Q2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment_C__Advanced_1
+﻿using Assignment_C__Advanced_1.Range_Q2;
+
+namespace Assignment_C__Advanced_1
 {
     internal class Program
     {
@@ -29,25 +31,41 @@
         }
         static void Main(string[] args)
         {
-            int[] arr = { 5, 1, 4, 2, 8 };
+            #region Q1
+            /*int[] arr = { 5, 1, 4, 2, 8 };
 
-            Console.WriteLine("Original array:");
+                Console.WriteLine("Original array:");
 
-            foreach (int item in arr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
+                foreach (int item in arr)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
 
-            BubbleSort(ref arr);
+                BubbleSort(ref arr);
 
-            Console.WriteLine("Sorted array:");
+                Console.WriteLine("Sorted array:");
 
-            foreach (int item in arr)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
+                foreach (int item in arr)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();*/
+            #endregion
+
+            #region Q2
+            /*var intRange = new Range<int>(5, 10);
+                Console.WriteLine($"Is 7 in range (5-10): {intRange.IsInRange(7)}"); 
+                Console.WriteLine($"Length of range (5-10): {intRange.Length()}");  
+
+                var doubleRange = new Range<double>(1.5, 4.5);
+                Console.WriteLine($"Is 3.2 in range (1.5-4.5): {doubleRange.IsInRange(3.2)}"); 
+                Console.WriteLine($"Length of range (1.5-4.5): {doubleRange.Length()}");    
+
+                var charRange = new Range<char>('a', 'z');
+                Console.WriteLine($"Is 'm' in range ('a'-'z'): {charRange.IsInRange('m')}"); 
+                Console.WriteLine($"Length of range ('a'-'z'): {charRange.Length()}");   */ 
+            #endregion
         }
     }
 }
